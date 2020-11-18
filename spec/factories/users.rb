38c3298aba9390228f9bd54133369fb590d43613
sutogone <batorukunn@yahoo.co.jp>
2020-11-18 16:41:07 +0900

    fakerの定義方法見直し

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    medical_record        {Faker::Number(digits: 3)}
+    medical_record        {Faker::Number.number(digits: 5)}
     email                 {Faker::Internet.free_email}
     password              {"test123"}
     password_confirmation {password}
