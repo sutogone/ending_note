6d1720b8ab734bca2f8d5fae3f10bcf2d2f61cec
sutogone <batorukunn@yahoo.co.jp>
2020-11-18 19:01:37 +0900

    患者基本情報のマイグレーションファイルを記述

--- db/migrate/20201118094239_create_basic_informations.rb
+++ db/migrate/20201118094239_create_basic_informations.rb
@@ -1,7 +1,15 @@
 class CreateBasicInformations < ActiveRecord::Migration[6.0]
   def change
     create_table :basic_informations do |t|
-
+      t.references  :user,        foreign_key: true
+      t.string      :last_name,                 null: false, default: ""
+      t.string      :first_name,                null: false, default: ""
+      t.string      :last_name_kana,            null: false, default: ""
+      t.string      :first_name_kana,           null: false, default: ""
+      t.date        :birthday,                  null: false
+      t.text        :address,                   null: false
+      t.text        :domicile
+      t.integer     :blood_type_id,              null: false
       t.timestamps
     end
   end
