aeb24ec97c76f8a786fdbbee65c51376ba5ad8c8
sutogone <batorukunn@yahoo.co.jp>
2020-11-18 17:57:56 +0900

    各種登録ページのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
-  resources :users
+  resources :users, only: [:index] do
+    resources :basic_information
+    resources :pets
+    resources :contacts
+    resources :funerals
+    resources :assets
+  end
 end
