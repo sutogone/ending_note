57a38d1f80af109f211b0e78daf45ebcf85694a3
sutogone <batorukunn@yahoo.co.jp>
2020-11-18 16:34:05 +0900

    userモデルのテストコードの準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/}
         
   with_options presence: true do
-  validates :medical_record, numericality: { only_integer: true }
+  validates :medical_record, uniqueness: true, numericality: { only_integer: true }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe "患者新規登録" do
+    context "登録がうまくいく時" do
+      it "必須項目が全て入力されていれば登録できること" do
+        expect(@user).to be_valid
+      end
+    end
+    
+    context "登録がうまくいかない時" do
+      it "medical_recordが空では登録できないこと" do
+        
+      end
+      it "emailが空では登録できないこと" do
+        
+      end
+      it "email登録の際＠が抜けていたら登録できないこと" do
+        
+      end
+      it "passwordが空では登録できないこと" do
+        
+      end
+      it "password_confirmationが空では登録ができないこと" do
+        
+      end
+      it "emailが重複していては登録できないこと" do
+        
+      end
+      it "passwordが5文字以下の場合登録できないこと" do
+        
+      end
+      it "passwordを半角英字だけ入力すると登録できないこと" do
+        
+      end
+      it "passwordを半角数字だけ入力すると登録できないこと" do
+        
+      end
+      it "passwordとpassword_confirmationの値が一致しないとき登録ができないこと" do
+        
+      end
+      it "medical_recordが数字以外だった場合登録できないこと" do
+        
+      end
+    end
+    
+  end
+  
 end
