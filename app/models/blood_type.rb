0bb4057bd7fb87eeb8d78dcd57c0b47313e4af3a
sutogone <batorukunn@yahoo.co.jp>
2020-11-19 17:38:20 +0900

    blood_typeのname属性を変更

--- app/models/blood_type.rb
+++ app/models/blood_type.rb
@@ -1,6 +1,6 @@
 class BloodType < ActiveHash::Base
   self.data = [
-    {id: 1, name: 'A型'}, {id: 2, name: 'B型'}, {id: 3, name: 'O型'},
-    {id: 4, name: 'AB型'}
+    {id: 1, type: 'A型'}, {id: 2, type: 'B型'}, {id: 3, type: 'O型'},
+    {id: 4, type: 'AB型'}
   ]
 end
