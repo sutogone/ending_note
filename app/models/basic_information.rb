8b718c0db3688b9b93431f1b20074a2d04ceeac8
sutogone <batorukunn@yahoo.co.jp>
2020-11-19 18:35:17 +0900

    blood_typeカラム周りの修正
    view:バリデーションエラーに引っかからない様に記述を修正
    model:バリデーションの見直し

--- app/models/basic_information.rb
+++ app/models/basic_information.rb
@@ -17,6 +17,6 @@ class BasicInformation < ApplicationRecord
     validates :prefecture_id,                     numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
-    validates :blood_type_id,                     numericality: { other_than: 1, message: "can't be blank" }
+    validates :blood_type_id,                     numericality: true
   end
 end
