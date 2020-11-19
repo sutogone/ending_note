ef9eb795cc0c89903cb5c2dd379aa5f9ad0692df
sutogone <batorukunn@yahoo.co.jp>
2020-11-19 17:34:27 +0900

    ストロングパラメータ見直し

--- app/controllers/basic_information_controller.rb
+++ app/controllers/basic_information_controller.rb
@@ -17,7 +17,7 @@ class BasicInformationController < ApplicationController
 
   private
   def basic_params
-    params.require(:basic_information).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, :address, :domicile, :blood_type_id).merge(user_id: current_user.id)
+    params.require(:basic_information).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, :postal_code, :prefecture_id, :city, :address, :building, :blood_type_id).merge(user_id: current_user.id)
   end
 
   def set_basic
