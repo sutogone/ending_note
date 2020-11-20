f973f9b4580887abd1269fed96493dd39a895206
sutogone <batorukunn@yahoo.co.jp>
2020-11-20 18:21:23 +0900

    pet1モデルのマイグレーションファイル編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_094239) do
+ActiveRecord::Schema.define(version: 2020_11_20_091702) do
 
   create_table "basic_informations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -30,6 +30,17 @@ ActiveRecord::Schema.define(version: 2020_11_18_094239) do
     t.index ["user_id"], name: "index_basic_informations_on_user_id"
   end
 
+  create_table "pets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "pet_name", default: "", null: false
+    t.string "pet_breed", default: "", null: false
+    t.integer "pet_age", null: false
+    t.text "pet_details", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,4 +55,5 @@ ActiveRecord::Schema.define(version: 2020_11_18_094239) do
   end
 
   add_foreign_key "basic_informations", "users"
+  add_foreign_key "pets", "users"
 end
