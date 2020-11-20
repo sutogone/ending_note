7487aff44f3348be0e90673e5822e92b0599f44d
sutogone <batorukunn@yahoo.co.jp>
2020-11-20 22:08:27 +0900

    ペット登録のテストコード実装
    アプリ全体のバリデーションの見直し
    バリデーションの見直しを行ったので設計も一部変更
    view:ペットの年齢を登録する欄に置いて半角数字を使用する様説明を改定

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,8 @@
 class Pet < ApplicationRecord
   belongs_to :user
-  
+  with_options presence: true do
+    validates :pet_name
+    validates :pet_breed
+    validates :pet_age,              numericality: true
+  end
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Pet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @pet = FactoryBot.build(:pet)
+  end
+
+  describe "ペット情報登録" do
+    context "ペット情報登録がうまくいく時" do
+      it "登録事項が全て入力されている時" do
+        expect(@pet).to be_valid
+      end
+    end
+      
+    context "ペット情報登録がうまくいかない時" do
+      it "pet_nameが空では登録できないこと" do
+        @pet.pet_name = ""
+        @pet.valid?
+        expect(@pet.errors.full_messages).to include("Pet name can't be blank")
+      end
+      it "pet_breedが空では登録できないこと" do
+        @pet.pet_breed = ""
+        @pet.valid?
+        expect(@pet.errors.full_messages).to include("Pet breed can't be blank")
+      end
+      it "pet_ageが空では登録できないこと" do
+        @pet.pet_age = ""
+        @pet.valid?
+        expect(@pet.errors.full_messages).to include("Pet age can't be blank")
+      end
+      it "pet_ageの値が数字以外では登録できないこと" do
+        @pet.pet_age = "aaa"
+        @pet.valid?
+        expect(@pet.errors.full_messages).to include("Pet age is not a number")
+      end
+    end
+  end
 end
