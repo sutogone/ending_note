ab8021ad8e88b31310ed401dbb86b4167c039dc0
sutogone <batorukunn@yahoo.co.jp>
2020-11-22 01:21:02 +0900

    funeralsコントローラー作成

---
+++ spec/helpers/funerals_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the FuneralsHelper. For example:
+#
+# describe FuneralsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe FuneralsHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
