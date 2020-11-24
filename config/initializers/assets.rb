ae963069bf99861c2dc85a5d2a234081f49a2572
sutogone <batorukunn@yahoo.co.jp>
2020-11-25 01:19:41 +0900

    ヘッダー作成

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( header.scss )
 Rails.application.config.assets.precompile += %w( users.scss )
 Rails.application.config.assets.precompile += %w( reset.scss )
 Rails.application.config.assets.precompile += %w( basic_information.scss )
